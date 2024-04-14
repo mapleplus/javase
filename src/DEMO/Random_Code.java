f1529b53d3b2d8e42705eee65ecca9c2c650c940
枫 <fx20031215@163.com>
2024-04-14 21:58:10 +0800

    Initial commit

---
+++ src/DEMO/Random_Code.java
@@ -0,0 +1,31 @@
+package DEMO;
+//生成随机验证码
+import java.util.Random;
+import java.util.Scanner;
+public class Random_Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入需要生成随机数的个数：");
+            int n = sc.nextInt();
+//            打印验证码
+            System.out.println(Code(n));
+            if (n == 0) {
+                break;
+            }
+        }
+    }
+    public static String Code(int n){
+        String code_1 = "";
+        String code_2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random rand = new Random();
+//        截取字符 形成验证码
+        for (int i = 0; i < n; i++) {
+//            随机生成code_2的索引
+            int r = rand.nextInt(code_2.length());
+//            使用索引将code_2对应的字符截取到code_1
+            code_1 += code_2.charAt(r);
+        }
+        return code_1;
+    }
+}
